d36156e25e86e2e28d48bb3e4dadf0e00f97451d
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-06 09:22:05 +0000

    day 6

---
+++ 2023/day6.js
@@ -0,0 +1,54 @@
+import { readFileSync } from "node:fs";
+const filename = "day6-input2.txt";
+const raw = readFileSync(filename).toString().trim().split("\n");
+
+console.log(raw.join("\n"));
+
+
+function marginOfError([time, record]) {
+
+    const wins = [];
+    for (let speed = 0; speed < time; speed++) {
+
+        const distance = speed * (time - speed);
+        if (speed % 1000 === 0) console.log(speed, time);
+        if (distance > record) wins.push(speed);
+
+    }
+    return wins.length;
+
+}
+
+(function part1() {
+
+
+    const parsed = raw
+        .map(line => line.split(" ").filter(x => x).slice(1).map(x => parseInt(x)))
+        .reduce((output, line, i) => i === 0 ? [...line] : line.map((x, j) => [output[j], x]), null)
+        ;
+
+    console.log("\nParsed", parsed);
+
+    const data = parsed
+        .map(marginOfError)
+        .reduce((a, b) => a * b)
+        ;
+
+    console.log("Part 1", data);
+
+}());
+
+(function part2() {
+
+    const parsed = raw
+        .map(x => x.split(":").map(y => y.trim()))
+        .map(([_label, numbers]) => parseInt(numbers.replace(/ /g, "")))
+        ;
+
+    const data = marginOfError(parsed)
+        ;
+
+    console.log(data);
+
+}())
+
