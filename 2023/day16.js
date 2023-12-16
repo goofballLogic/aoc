47c7e44db6f29802d9d9604b2391f627ad6aa9a1
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-16 12:32:47 +0000

    TCR

--- 2023/day16.js
+++ 2023/day16.js
@@ -9,9 +9,15 @@ const raw = readFileSync(`day${day}-input${input}.txt`).toString().trim().split(
 const N = 0, E = 1, S = 2, W = 3;
 
 const transformBeam = (fromDirection, tile) =>
-    [N, S].includes(fromDirection) ? [fromDirection] : [N, S]
+    tile === "|"
+        ? splitNS(fromDirection)
+        : [fromDirection]
     ;
 test("transform beam: E|NS", [N, S], () => transformBeam(E, "|"));
 test("transform beam: W|NS", [N, S], () => transformBeam(W, "|"));
 test("transform beam: N|N", [N], () => transformBeam(N, "|"));
 test("transform beam: S|S", [S], () => transformBeam(S, "|"));
+function splitNS(fromDirection) {
+    return [N, S].includes(fromDirection) ? [fromDirection] : [N, S];
+}
+
