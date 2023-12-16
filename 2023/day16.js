a09ec98ce25adba2c4df9a89beb9ec0c004cd2fc
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-16 20:35:37 +0000

    TCR

--- 2023/day16.js
+++ 2023/day16.js
@@ -146,7 +146,7 @@ const part1 = map =>
         .pipe(([_paths, count]) => count + 1)
     ;
 
-test(["..", ".. part1"].join("\n"), 2, x => part1(x.split("\n")));
+test(["..", ".. part1"].join("\n"), 2, x => part1(x.split(" ")[0].split("\n")));
 test([".|", ".."].join("\n"), 3); //, x => part1(x.split("\n")));
 test([".|", "./"].join("\n"), 4);
 test([".|", "\\/"].join("\n"), 4);
