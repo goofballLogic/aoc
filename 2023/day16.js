0be6cdbd08a8d0fd0214207eed8681f6e97b9065
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-16 12:35:10 +0000

    TCR

--- 2023/day16.js
+++ 2023/day16.js
@@ -12,14 +12,25 @@ const splitNS = fromDirection =>
     [N, S].includes(fromDirection) ? [fromDirection] : [N, S]
     ;
 
+const splitWE = fromDirection =>
+    [W, E].includes(fromDirection) ? [fromDirection] : [W, E]
+    ;
+
 const transformBeam = (fromDirection, tile) =>
     tile === "|"
         ? splitNS(fromDirection)
-        : [fromDirection]
+        : tile === "-"
+            ? splitWE(fromDirection)
+            : [fromDirection]
     ;
 test("transform beam: E|NS", [N, S], () => transformBeam(E, "|"));
 test("transform beam: W|NS", [N, S], () => transformBeam(W, "|"));
 test("transform beam: N|N", [N], () => transformBeam(N, "|"));
 test("transform beam: S|S", [S], () => transformBeam(S, "|"));
+
+test("transform beam: E-E", [E], () => transformBeam(E, "-"));
+test("transform beam: W-W", [W], () => transformBeam(W, "-"));
+test("transform beam: N-WE", [W, E], () => transformBeam(N, "-"));
+test("transform beam: S-WE", [W, E], () => transformBeam(S, "-"));
 
 
