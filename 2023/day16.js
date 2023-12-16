3ac2e367f07c841fc7b7b9346b2ffa4f7d5c5e2f
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-16 21:47:44 +0000

    TCR

--- 2023/day16.js
+++ 2023/day16.js
@@ -201,6 +201,6 @@ test(["..", ".. part1"].join("\n"), 2, x => part1(part1Testdata(x)));
 test([".|", ".. part1"].join("\n"), 3, x => part1(part1Testdata(x)));
 test([".|", "./"].join("\n"), 4, x => part1(part1Testdata(x)));
 test([".|", "\\/"].join("\n"), 4, x => part1(part1Testdata(x)));
-// test([".|.", "/-\\", "..."].join("\n"), 7, x => part1(part1Testdata(x)));
+test([".|.", "/-\\", "..."].join("\n"), 7, x => part1(part1Testdata(x)));
 // test([".|\\", ".\\/"].join("\n"), 5, x => part1(part1Testdata(x)));
 
