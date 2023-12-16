56ffcbd3040a1c68211c5d65b0919795d9a06368
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-16 21:51:36 +0000

    TCR

--- 2023/day16.js
+++ 2023/day16.js
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 import { test } from "./test.js";
 import "./evil.js";
 
-const input = 1;
+const input = 2;
 const day = 16;
 const raw = readFileSync(`day${day}-input${input}.txt`).toString().trim().split("\n");
 
