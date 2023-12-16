0fd55fa859cfee9d842625a75bad38c144d5ba14
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-16 22:35:35 +0000

    TCR

--- 2023/day16.js
+++ 2023/day16.js
@@ -234,13 +234,13 @@ test([".|\\", ".\\/"].join("\n"), 5, x => part1(parseTestData(x)));
 const part2 = map =>
     Math.max(
         map.reduce((max, line, y) =>
-            console.log({ y }) || Math.max(
+            Math.max(
                 max,
                 countWalk([E, y, -1], map),
                 countWalk([W, y, line.length], map)
             ), 0),
         map[0].split("").reduce((max, _, x) =>
-            console.log({ x }) || Math.max(
+            Math.max(
                 max,
                 countWalk([S, x, 0], map),
                 countWalk([N, x, map[0].length], map)
