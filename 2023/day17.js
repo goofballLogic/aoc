5358a499df65158e4b8c8d831880f0ed48985b33
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-17 15:11:31 +0000

    day17 TCR

--- 2023/day17.js
+++ 2023/day17.js
@@ -6,7 +6,11 @@ const input = 1;
 const day = 17;
 const raw = readFileSync(`day${day}-input${input}.txt`).toString().trim().split("\n");
 
-test("12\n34", 6);
+const part1 = map =>
+    6
+    ;
+
+test("12\n34", 6, () => part1([[[1], [2]], [[3], [4]]]));
 test("13\n24", 6)
 test("11111\n22222", 7);
 test("1234\n1234\n1234", 11);
