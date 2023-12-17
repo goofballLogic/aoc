e258b6dc46705e5bc5ca927916ad2a23727e1387
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-17 14:58:23 +0000

    TCR

---
+++ 2023/day17.js
@@ -0,0 +1,9 @@
+import { readFileSync } from "node:fs";
+import { test } from "./test.js";
+import "./evil.js";
+
+const input = 1;
+const day = 17;
+const raw = readFileSync(`day${day}-input${input}.txt`).toString().trim().split("\n");
+
+console.log(raw);
---
+++ 2023/tcr.sh
@@ -0,0 +1,1 @@
+node $1 && (git add . -A && git commit -m TCR) || git reset --hard
