bfeca9770cccd49e4cad8cc7805469f092352218
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-18 14:24:26 +0000

    day17 TCR

--- 2023/day17.js
+++ 2023/day17.js
@@ -6,24 +6,10 @@ const input = 1;
 const day = 17;
 const raw = readFileSync(`day${day}-input${input}.txt`).toString().trim().split("\n");
 
-const walk = ({ map, pos: [y, x], dest }) => {
+const N = 0, E = 1, S = 2, W = 3;
 
-    // add one to x
-    const nextPos = [y, x + 1];
-    const lastTurned = 1;
-    return 2;
 
-};
-
-const part1 = map =>
-    walk({
-        map,
-        pos: [0, 0],
-        dest: [map.length - 1, map[0].length]
-    })
-    ;
-
-test("12", 2, () => part1(["12"]));
+test("12", 2); //, () => part1(["12"]));
 test("12\n34", 6); //, () => part1(["12", "34"]));
 test("13\n24", 6); //, () => part1(["13", "24"]));
 test("11111\n22222", 7);
