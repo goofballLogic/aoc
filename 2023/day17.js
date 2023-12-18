325c2226551a68a4c0591fe07202b48087b4de35
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-18 14:52:29 +0000

    day17 TCR

--- 2023/day17.js
+++ 2023/day17.js
@@ -7,13 +7,18 @@ const day = 17;
 const raw = readFileSync(`day${day}-input${input}.txt`).toString().trim().split("\n");
 
 const N = 0, E = 1, S = 2, W = 3;
+const compass = [N, E, S, W];
 
 const straight = ({ dir }) => dir;
+
+const right = ({ dir }) => compass[dir + 1] || N;
 
 const nextPosForDir = (dir, [y, x]) =>
     dir === E
         ? [y, x + 1]
-        : [y, x]
+        : dir === S
+            ? [y + 1, x]
+            : [y, x]
     ;
 
 const mappedState = (map, state) =>
@@ -26,14 +31,30 @@ const nextStateForDir = (dir, map, state) =>
 const nextStates = (map, state) =>
     [
         nextStateForDir(straight(state), map, state),
-        //goRight(state)
-    ].filter(x => x);
+        nextStateForDir(right(state), map, state)
+    ].filter(({ w }) => w);
 
-test("12 E,0,0 next states", [{ dir: E, pos: [0, 1], w: 2 }], () => nextStates([[1, 2]], { dir: E, pos: [0, 0] }));
-test("12\n34 E,0,0 next states", [{ dir: E, pos: [0, 1], w: 2 }, { dir: S, pos: [1, 0], w: 3 }]);
-test("12", 2); //, () => part1(["12"]));
-test("12\n34", 6); //, () => part1(["12", "34"]));
-test("13\n24", 6); //, () => part1(["13", "24"]));
-test("11111\n22222", 7);
-test("1234\n1234\n1234", 11);
-test("19111\n19191\n19191\n11191\n99991", 14);
+test(
+    "12 E,0,0 next states",
+    [{ dir: E, pos: [0, 1], w: 2 }],
+    () => nextStates([[1, 2]], { dir: E, pos: [0, 0] })
+);
+test(
+    "12\n34 E,0,0 next states",
+    [{ dir: E, pos: [0, 1], w: 2 }, { dir: S, pos: [1, 0], w: 3 }],
+    () => nextStates([[1, 2], [3, 4]], { dir: E, pos: [0, 0] })
+);
+test("13\n24"); //, () => part1(["13", "24"]));
+test("11111\n22222");
+test("1234\n1234\n1234");
+test("19111\n19191\n19191\n11191\n99991");
+
+
+
+
+// test("12", 2); //, () => part1(["12"]));
+// test("12\n34", 6); //, () => part1(["12", "34"]));
+// test("13\n24", 6); //, () => part1(["13", "24"]));
+// test("11111\n22222", 7);
+// test("1234\n1234\n1234", 11);
+// test("19111\n19191\n19191\n11191\n99991", 14);
