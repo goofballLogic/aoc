7a70b4b33e163e7312661d2412839d9fb112fd74
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-18 15:10:04 +0000

    day17 TCR

--- 2023/day17.js
+++ 2023/day17.js
@@ -10,15 +10,17 @@ const N = 0, E = 1, S = 2, W = 3;
 const compass = [N, E, S, W];
 
 const straight = ({ dir }) => dir;
-
 const right = ({ dir }) => compass[dir + 1] || N;
+const left = ({ dir }) => compass[dir - 1] || N;
 
 const nextPosForDir = (dir, [y, x]) =>
     dir === E
         ? [y, x + 1]
         : dir === S
             ? [y + 1, x]
-            : [y, x]
+            : dir === W
+                ? [y, x - 1]
+                : [y - 1, x] // N
     ;
 
 const mappedState = (map, state) =>
@@ -31,7 +33,8 @@ const nextStateForDir = (dir, map, state) =>
 const nextStates = (map, state) =>
     [
         nextStateForDir(straight(state), map, state),
-        nextStateForDir(right(state), map, state)
+        nextStateForDir(right(state), map, state),
+        nextStateForDir(left(state), map, state)
     ].filter(({ w }) => w);
 
 test(
@@ -56,7 +59,8 @@ test(
 );
 test(
     "1234\n1234\n1234 W,0,0 next states",
-    [{ dir: S, pos: [1, 0], w: 1 }]
+    [{ dir: S, pos: [1, 0], w: 1 }],
+    () => nextStates([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]], { dir: W, pos: [0, 0] })
 );
 test("19111\n19191\n19191\n11191\n99991");
 
