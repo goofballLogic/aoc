a01ef594376cc4afee235453f24a4ac88bda9384
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-19 09:47:10 +0000

    day17 TCR

--- 2023/day17.js
+++ 2023/day17.js
@@ -136,7 +136,7 @@ console.log(raw.map(line => line.join("")).join("\n"));
 console.log(startingStates);
 
 const cheapestNode = stateMap =>
-    Object.entries(stateMap).reduce((apair, bpair) => console.log(apair, bpair) || bpair[1].ws < apair[1].ws ? bpair : apair)[0];
+    Object.entries(stateMap).reduce((apair, bpair) => bpair[1].ws < apair[1].ws ? bpair : apair)[0];
 
 test("Cheapest node", "1_0_0_0", () => cheapestNode(startingStates));
 test("Cheapest node", "2_0_0_0", () => cheapestNode({ "1_0_0_0": { ws: 2 }, "2_0_0_0": { ws: 1 } }));
