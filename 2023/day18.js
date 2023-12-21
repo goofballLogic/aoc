0412cc4025cb0ec68742a3ead0cbc120c65e84b6
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-21 16:46:11 +0000

    day18 TCR

--- 2023/day18.js
+++ 2023/day18.js
@@ -135,3 +135,18 @@ const part1 = instructions =>
     ;
 
 console.log("Part 1", part1(data));
+
+const decodeInstructionParts = (meters, dir) =>
+    [
+        dir === 0 ? "R" : dir === 1 ? "D" : dir === 2 ? "L" : "U",
+        parseInt(meters, 16)
+    ]
+    ;
+
+const decodeInstruction = instruction =>
+    decodeInstructionParts(
+        Array.from(/(.{5})(.)\)/.exec(instruction)).slice(1)
+    )
+    ;
+
+test("(#70c710)");
