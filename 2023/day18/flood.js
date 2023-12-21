7dbb9e1a777ca01e9a371ef7cf3cbe1fa394afbd
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-21 10:11:20 +0000

    day18/flood.js TCR

--- 2023/day18/flood.js
+++ 2023/day18/flood.js
@@ -8,4 +8,4 @@ export function flood({ map: { lines }, empty = ".", x = -1, y = -1 }) {
 const pairs = data => data.split(",").map(pair => pair.split("").map(x => parseInt(x)));
 
 test("...\n.#.\n...", pairs("00,01,02,10,12,20,21,22"));
-test(".......\n.#####.\n.#.....\n.#..##.\n.#...#.\n.#####.\n.......");
+test(".......\n.#####.\n.#.....\n.#.###.\n.#...#.\n.#####.\n.......");
