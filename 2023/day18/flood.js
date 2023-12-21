2baa22d97445bc1edaf00e342256a3648ef36e83
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-21 10:29:19 +0000

    day18/flood.js TCR

--- 2023/day18/flood.js
+++ 2023/day18/flood.js
@@ -1,5 +1,19 @@
 import { test } from "../test.js";
 
+function around(maxx, maxy, x, y) {
+    const ret = [];
+    for (let xx = Math.max(0, x - 1); xx < Math.min(maxx, x + 1); x++) {
+        for (let yy = Math.max(0, y - 1); yy < Math.min(maxy, y + 1); y++)
+            ret.push([xx, yy]);
+    }
+    return ret;
+}
+
+test(
+    "10,10,0,0 around",
+    [[0, 1], [1, 0], [1, 1]]
+);
+
 export function flood({ map: { lines }, empty = ".", x = 0, y = 0 }) {
 
 
