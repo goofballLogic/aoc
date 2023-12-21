f1a0e94508e881656f33710f98dc517a5d923806
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-21 11:11:25 +0000

    day18/flood.js TCR

--- 2023/day18/flood.js
+++ 2023/day18/flood.js
@@ -23,12 +23,17 @@ test(
     [[9, 9], [9, 10], [10, 9]],
     () => around(10, 10, 10, 10)
 );
+test(
+    "2,2,1,1 around",
+    [[0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]],
+    () => around(2, 2, 1, 1)
+)
 
 export function flood({ map, empty = ".", x = 0, y = 0 }) {
 
     const maxx = map[0].length;
     const maxy = map[1].length;
-    const visited = [];
+    const visited = new Set();
     const filled = [];
     const visitable = [[x, y]];
     let visitablei = 0;
@@ -38,11 +43,11 @@ export function flood({ map, empty = ".", x = 0, y = 0 }) {
         visitablei++;
 
         const key = `${vx}_${vy}`;
-        if (!visited.includes(key)) {
+        if (!visited.has(key)) {
 
             if (map[vy]?.[vx] === empty) filled.push(key);
             visitable.push(...around(maxx, maxy, vx, vy));
-            visited.push(key);
+            visited.add(key);
 
         }
 
@@ -58,7 +63,8 @@ test(
     pairs("00,01,02,10,12,20,21,22"),
     () => flood({ map: ["...", ".#.", "..."] })
 );
-test(
-    ".......\n.#####.\n.#.....\n.#.####\n.#.#..#\n.#....#\n.######",
-    pairs("00,01,02,03,04,05,06,10,16,20,22,23,24,25,26,30,32,40,42,44,45,50,52,53,54,55,60")
-);
+// test(
+//     ".......\n.#####.\n.#.....\n.#.####\n.#.#..#\n.#....#\n.######",
+//     pairs("00,01,02,03,04,05,06,10,16,20,22,23,24,25,26,30,32,40,42,44,45,50,52,53,54,55,60"),
+//     () => flood({ map: ".......\n.#####.\n.#.....\n.#.####\n.#.#..#\n.#....#\n.######".split("\n") })
+// );
