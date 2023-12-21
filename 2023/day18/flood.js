415fc089e95c5012a4527008cc106b1dd38804cc
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-21 10:04:40 +0000

    day18/flood.js TCR

---
+++ 2023/day18/flood.js
@@ -0,0 +1,8 @@
+import { test } from "../test.js";
+
+export function flood({ map: { lines }, empty = ".", x = -1, y = -1 }) {
+
+
+}
+
+test("...\n.#.\n...", [[0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]]);
