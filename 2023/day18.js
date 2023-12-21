fe960c9da675c085f4058c65792ce27dce8a2145
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-21 12:46:31 +0000

    day18 TCR

--- 2023/day18.js
+++ 2023/day18.js
@@ -51,7 +51,7 @@ function draw(instructions) {
 }
 
 test("U1", [[0, 0], [0, -1]], () => draw([["U", 1]]));
-test("U1 R1", [[0, 0], [0, -1], [1, -1]]);
+test("U1 R1", [[0, 0], [0, -1], [1, -1]], () => draw([["U", 1], ["R", 1]]));
 
 // draw ma
 // count map
