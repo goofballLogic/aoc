701a619c0c79951f3fd7b0f447f56038fb8d8fb8
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-22 10:05:28 +0000

    day18 TCR

--- 2023/day18.js
+++ 2023/day18.js
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 import { test } from "./test.js";
 import "./evil.js";
 import { flood } from "./day18/flood.js";
-
+import { draw } from "./draw.js";
 
 const input = 1;
 const day = 18;
@@ -18,41 +18,6 @@ const data = raw
     ;
 
 console.log(data);
-
-function go(x, y, a, i) {
-    switch (a) {
-        case "U":
-            return [x, y - i];
-        case "D":
-            return [x, y + i];
-        case "L":
-            return [x - i, y];
-        case "R":
-            return [x + i, y];
-        default:
-            return [x, y];
-    }
-}
-
-test("go 0 0 U 1", [0, -1], () => go(0, 0, "U", 1));
-test("go 0 0 D 2", [0, 2], () => go(0, 0, "D", 2));
-test("go 0 0 L 3", [-3, 0], () => go(0, 0, "L", 3));
-test("go 0 0 R 4", [4, 0], () => go(0, 0, "R", 4));
-
-function draw(instructions) {
-
-    const map = [[0, 0]];
-    let [x, y] = [0, 0];
-    for (const instruction of instructions) {
-        [x, y] = go(x, y, ...instruction);
-        map.push([x, y]);
-    }
-    return map;
-
-}
-
-test("U1", [[0, 0], [0, -1]], () => draw([["U", 1]]));
-test("U1 R1", [[0, 0], [0, -1], [1, -1]], () => draw([["U", 1], ["R", 1]]));
 
 const calcDimensions = coords =>
     coords
---
+++ 2023/draw.js
@@ -0,0 +1,33 @@
+import { test } from "./test.js";
+
+function go(x, y, a, i) {
+    switch (a) {
+        case "U":
+            return [x, y - i];
+        case "D":
+            return [x, y + i];
+        case "L":
+            return [x - i, y];
+        case "R":
+            return [x + i, y];
+        default:
+            return [x, y];
+    }
+}
+test("go 0 0 U 1", [0, -1], () => go(0, 0, "U", 1));
+test("go 0 0 D 2", [0, 2], () => go(0, 0, "D", 2));
+test("go 0 0 L 3", [-3, 0], () => go(0, 0, "L", 3));
+test("go 0 0 R 4", [4, 0], () => go(0, 0, "R", 4));
+export function draw(instructions) {
+
+    const map = [[0, 0]];
+    let [x, y] = [0, 0];
+    for (const instruction of instructions) {
+        [x, y] = go(x, y, ...instruction);
+        map.push([x, y]);
+    }
+    return map;
+
+}
+test("U1", [[0, 0], [0, -1]], () => draw([["U", 1]]));
+test("U1 R1", [[0, 0], [0, -1], [1, -1]], () => draw([["U", 1], ["R", 1]]));
