19c15f2f7591fa2b28a7e035e8742074cf74b06f
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-22 10:06:45 +0000

    day18 TCR

--- 2023/day18.js
+++ 2023/day18.js
@@ -3,6 +3,7 @@ import { test } from "./test.js";
 import "./evil.js";
 import { flood } from "./day18/flood.js";
 import { draw } from "./day18/draw.js";
+import { mapCoords } from "./day18/mapCoords.js";
 
 const input = 1;
 const day = 18;
@@ -18,61 +19,6 @@ const data = raw
     ;
 
 console.log(data);
-
-const calcDimensions = coords =>
-    coords
-        .reduce((prev, next) => [
-            Math.min(prev[0], next[0]),
-            Math.min(prev[1], next[1]),
-            Math.max(prev[2], next[0]),
-            Math.max(prev[3], next[1])
-        ], [Infinity, Infinity, -Infinity, -Infinity])
-    ;
-
-test(
-    "U1 calcDimension",
-    [0, -1, 0, 0],
-    () => calcDimensions(draw([["U", 1]]))
-);
-
-const remapCoords = (coords, dx, dy) =>
-    coords
-        .map(([x, y]) => [x + dx, y + dy])
-    ;
-
-const mapCoords = coords => {
-
-    const dimensions = calcDimensions(coords);
-    const width = dimensions[2] - dimensions[0] + 1;
-    const height = dimensions[3] - dimensions[1] + 1;
-    const map = Array(height).fill("").map(() => Array(width).fill("."));
-    const remappedCoords = remapCoords(coords, dimensions[0] * -1, dimensions[1] * -1);
-    for (let i = 1; i < remappedCoords.length; i++) {
-
-        let [x1, y1] = remappedCoords[i - 1];
-        let [x2, y2] = remappedCoords[i];
-        if (x1 > x2) [x1, x2] = [x2, x1];
-        if (y1 > y2) [y1, y2] = [y2, y1];
-        for (let x = x1; x <= x2; x++)
-            for (let y = y1; y <= y2; y++)
-                map[y][x] = "#";
-
-    }
-    return map;
-
-}
-    ;
-
-test(
-    "U1 L1 mapCoords",
-    [["#", "#"], [".", "#"]],
-    () => mapCoords(draw([["U", 1], ["L", 1]]))
-);
-test(
-    "U1 L1 D2 R2 mapCoords",
-    [["#", "#", "."], ["#", "#", "."], ["#", "#", "#"]],
-    () => mapCoords(draw([["U", 1], ["L", 1], ["D", 2], ["R", 2]]))
-);
 
 const part1 = instructions =>
     instructions
