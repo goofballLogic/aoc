f8896a603fa9cbc2c0326fbaa9c5cd8cc6865218
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-12-22 20:15:40 +0000

    day18 TCR

--- 2023/day18.js
+++ 2023/day18.js
@@ -30,7 +30,7 @@ const part1 = instructions =>
                     .pipe(map => [map, flood({ map, x: -1, y: -1 })])
                     .tap(([map, flooded]) => console.log(map, flooded))
                     .pipe(([map, flooded]) => [map, decompressCoordinates(flooded, compressed)])
-                    .tap(([map, flooded]) => console.log(map, flooded))
+                    .tap(([map, flooded]) => console.log(map.map(line => line.join("")), flooded))
                     .pipe(([map, flooded]) =>
                         map[0].length * map.length
                         - flooded.length
